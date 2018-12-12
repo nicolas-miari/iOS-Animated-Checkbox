c83dcdfed612621f8d91167ad2543dc07fc456b7
Nicolás Miari <nicolasmiari@gmail.com>
2018-12-12 19:05:08 +0900

    Cleanup

--- AnimatedCheckbox/AnimatedCheckbox/GalleryViewController.swift
+++ AnimatedCheckbox/AnimatedCheckbox/GalleryViewController.swift
@@ -12,12 +12,6 @@ class GalleryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
